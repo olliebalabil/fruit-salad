912fad3ce78576b4af31b7ce57db3c581afd324a
olliebalabil <oliverbalabil@gmail.com>
2023-09-11 12:06:41 +0100

    Ready to use APIs

--- js/script.js
+++ js/script.js
@@ -1,3 +1,19 @@
 const fruitForm = document.querySelector("#inputSection form");
-fruitForm.addEventListener("submit", e => console.log(e));
-console.log(fruitForm);
+const fruitList = document.querySelector("#fruitSection ul")
+
+const deleteFruit = e => {
+  e.target.remove();
+}
+
+const addFruit = (fruit) => {
+  const li = document.createElement("li");
+  li.textContent = fruit;
+  li.addEventListener("click",deleteFruit, {once:true})
+  fruitList.appendChild(li);
+}
+
+fruitForm.addEventListener("submit", e => {
+  e.preventDefault();
+  addFruit(e.target.fruitInput.value);
+  e.target.fruitInput.value = "";
+});
